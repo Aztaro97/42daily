0ef9a60f7127d32ff4f9c24a206c9b6017e847b0
aztaro97 <aztaro97@gmail.com>
2023-04-15 02:25:26 +0400

    Updated twin.macro to v^3.3.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,20 @@
 import * as React from "react";
 import GlobalStyles from "@/styles/globalStyles";
 
+import "@/styles/globals.css";
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <>
-      <html lang="en">
-        <head />
-        <body className="min-h-screen">
-            <GlobalStyles />
-            {children}
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <head />
+      <body className="min-h-screen">
+        <GlobalStyles />
+        {children}
+      </body>
+    </html>
   );
 }
